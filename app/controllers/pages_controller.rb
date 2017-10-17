056095ec3c5b7654ada21ba6e18750bd1d695a16
CharlesEric Legras <c.legras@me.com>
2017-10-17 14:57:23 +0200

    home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @profiles = Profile.all
     @posts = Post.last(6)
   end
 end
